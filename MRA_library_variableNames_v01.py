4cb3cf8cd7967d55ea328a79a597ea82063dc9fb
Andres <mendezdelrioa@gmail.com>
2021-12-04 02:09:10 +0100

    Formateo Automatico Python

--- MRA_library_variableNames_v01.py
+++ MRA_library_variableNames_v01.py
@@ -1,4 +1,4 @@
-#Libreria de variables naming 
+#Libreria de variables naming
 
 
 ###  ID Propio
@@ -10,17 +10,17 @@ id_alumno = "MRA"
 ### Ejercicio ID
 ###
 exercise_id = {
-"script" : "scr",
-"story" : "str",
-"animatic" : "anmtc",
-"colorKeys" : "ck",
-"layout" : "layout",
-"animation" : "anim",
-"cache" : "cache",
-"FX" : "fx",
-"lightning" : "lg",
-"compositing" : "comp",
-"rendering" : "rdrng"
+    "script": "scr",
+    "story": "str",
+    "animatic": "anmtc",
+    "colorKeys": "ck",
+    "layout": "layout",
+    "animation": "anim",
+    "cache": "cache",
+    "FX": "fx",
+    "lightning": "lg",
+    "compositing": "comp",
+    "rendering": "rdrng"
 }
 
 
@@ -28,55 +28,53 @@ exercise_id = {
 ###  Asset Type
 ###
 asset_type = {
-"character" : "ch",
-"prop" : "pr",
-"set" : "set",
-"camera" : "cm",
-"addon" : "ad",
-"generic" : "gn"
+    "character": "ch",
+    "prop": "pr",
+    "set": "set",
+    "camera": "cm",
+    "addon": "ad",
+    "generic": "gn"
 }
 
 ###
 ###  Process ID
 ###
 process_id = {
-"modelado" : "mod",
-"animRig" : "anim",
-"layoutRig" : "layout",
-"rig" : "rig",
-"cloth" : "cloth",
-"hair" : "hair",
-"shading" : "shd"
+    "modelado": "mod",
+    "animRig": "anim",
+    "layoutRig": "layout",
+    "rig": "rig",
+    "cloth": "cloth",
+    "hair": "hair",
+    "shading": "shd"
 }
 ###
 ###  Maya Scene Naming
 ###
 
 
-
-
 naming_maya = {
-"geometry" : "geo",
-"group" : "grp",
-"control" : "ctl",
-"locator"  : "lct",
-"spline" : "spl",
-"joint" : "jnt",
-"controlIK" : "cik",
-"controlFK" : "cfk",
-"cluster" : "cls",
-"jointMain" : "main",
-"jointSkin" : "skin",
-"light" : "lgt",
-"camera" : "cam",
-"imagePlane" : "imp",
-"orientConstraint" : "ons",
-"pointConstraint" : "pns",
-"aimConstraint" : "ans",
-"parentConstraint" : "pans",
-"scaleConstraint" : "sns",
-"ikHandle" : "ikh",
-"ikEffector" : "eft"
+    "geometry": "geo",
+    "group": "grp",
+    "control": "ctl",
+    "locator": "lct",
+    "spline": "spl",
+    "joint": "jnt",
+    "controlIK": "cik",
+    "controlFK": "cfk",
+    "cluster": "cls",
+    "jointMain": "main",
+    "jointSkin": "skin",
+    "light": "lgt",
+    "camera": "cam",
+    "imagePlane": "imp",
+    "orientConstraint": "ons",
+    "pointConstraint": "pns",
+    "aimConstraint": "ans",
+    "parentConstraint": "pans",
+    "scaleConstraint": "sns",
+    "ikHandle": "ikh",
+    "ikEffector": "eft"
 }
 
 ###
@@ -84,14 +82,14 @@ naming_maya = {
 ###
 
 location_flags = {
-"irrelevant" : "x",
-"center" : "c",
-"left" : "l",
-"right" : "r",
-"front" : "f",
-"back" : "b",
-"up" : "u",
-"down" : "d"
+    "irrelevant": "x",
+    "center": "c",
+    "left": "l",
+    "right": "r",
+    "front": "f",
+    "back": "b",
+    "up": "u",
+    "down": "d"
 }
 
-group_exceptions = ["geo","rig","ctl","toolkit","skin","lct"]
+group_exceptions = ["geo", "rig", "ctl", "toolkit", "skin", "lct"]
